e2e925c6cb104457133cac03a5e0bac9ff1c7cd7
NatalinaViskovatykh <Natalia_viskovatykh@epam.com>
2024-02-06 22:11:07 +0200

    Add Service Bus

--- fa-import-service/blob-import-products-from-file/index.ts
+++ fa-import-service/blob-import-products-from-file/index.ts
@@ -1,11 +1,18 @@
 import { AzureFunction, Context } from "@azure/functions";
 import * as csv from 'csv-parser';
 import { BlobServiceClient } from "@azure/storage-blob";
+import { ServiceBusClient } from "@azure/service-bus";
 require('dotenv').config();
 
 const blobTrigger: AzureFunction = async function(context: Context, myBlob: any): Promise<void> {
     const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AzureWebJobsStorage);
-    const containerClient = blobServiceClient.getContainerClient('uploaded');
+    const containerClient = blobServiceClient.getContainerClient('my-container');
+
+    const serviceBusConnectionString = process.env.ServiceBusConnectionString;
+    const queueName = "natalina_servicebus_queue";
+
+    const serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+    const sender = serviceBusClient.createSender(queueName);
 
     context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
 
@@ -17,9 +24,10 @@ const blobTrigger: AzureFunction = async function(context: Context, myBlob: any)
     readableStream
         .pipe(csv())
         .on('data', row => {
-            context.log(`Logged row: ${JSON.stringify(row)}`);
+            sender.sendMessages({body : JSON.stringify(row)});
         })
         .on('end', ()=> {
+            sender.close();
             context.log('CSV Blob file reading completed.');
         });
 };
--- fa-products-service-sand-ne-001/dist/http-get-product-by-id/index.js
+++ fa-products-service-sand-ne-001/dist/http-get-product-by-id/index.js
@@ -9,15 +9,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const constant_1 = require("../constant");
+const cosmos_1 = require("@azure/cosmos");
+require('dotenv').config();
+const key = process.env.COSMOS_KEY;
+const endpoint = process.env.COSMOS_ENDPOINT;
+const databaseName = `products-db`;
+const container = { 'products': 'products', 'stock': 'stock' };
+const cosmosClient = new cosmos_1.CosmosClient({ endpoint, key });
+const database = cosmosClient.database(databaseName);
+const containerProduct = database.container(container.products);
 const httpTrigger = function (context, req) {
     return __awaiter(this, void 0, void 0, function* () {
         context.log('HTTP trigger function http-get-product-by-id processed a request.');
+        context.log(req);
         const { productId } = req.params;
-        const product = constant_1.default === null || constant_1.default === void 0 ? void 0 : constant_1.default.find(item => item.id === productId);
+        const results = yield containerProduct.items
+            .query({
+            query: "SELECT * FROM c WHERE  c.id = @id",
+            parameters: [{ name: "@id", value: productId }]
+        })
+            .fetchAll();
+        context.log(results.resources);
         context.res = {
-            status: product ? 200 : 404,
-            body: product ? product : console.error("Product not found")
+            status: results.resources ? 200 : 404,
+            body: results.resources ? results.resources : context.log("Product not found")
         };
     });
 };
